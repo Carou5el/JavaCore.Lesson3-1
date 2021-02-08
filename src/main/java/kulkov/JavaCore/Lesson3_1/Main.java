60f2cd6f585e4fd90d71196352b04572edb18ddd
Carou5el <garrydeepcool@outlook.com>
2021-02-08 17:30:47 +0500

    Add a test source code.

--- src/main/java/kulkov/JavaCore/Lesson3_1/Main.java
+++ src/main/java/kulkov/JavaCore/Lesson3_1/Main.java
@@ -1,7 +1,143 @@
 package kulkov.JavaCore.Lesson3_1;
 
-public class Main {
-    public static void main(String[] args) {
+import java.util.Random;
+import java.util.Scanner;
 
+public class Main {
+/*
+    Переделать проверку победы, чтобы она не была реализована просто набором условий.
+    Попробовать переписать логику проверки победы, чтобы она работала для поля 5х5 и количества фишек 4 в линию.
+*** Доработать искусственный интеллект, чтобы он мог блокировать ходы игрока, и пытаться выиграть сам.
+
+ */
+    private static char[][] field;
+    private static final char DOT_HUMAN = 'X';
+    private static final char DOT_AI = 'O';
+    private static final char DOT_EMPTY = '.';
+    private static final Scanner SCANNER = new Scanner(System.in);
+    private static final Random RANDOM = new Random();
+    private static int fieldSizeX;
+    private static int fieldSizeY;
+
+    public static void main(String[] args) {
+        while (true) {
+            initField();
+            printField();
+
+            while (true) {
+                humanTurn();
+                printField();
+                if (checkGame(DOT_HUMAN, "Human wins!!!")) break;
+                aiTurn();
+                printField();
+                if (checkGame(DOT_AI, "AI win!!!")) break;
+            }
+            System.out.println("Wanna play again?");
+            if (!SCANNER.next().equals("y")){
+                SCANNER.close();
+                break;
+            }
+        }
+    }
+
+
+    private static void humanTurn() {
+        int x;
+        int y;
+
+        do {
+            System.out.print("Введите координаты хода Х и У от 1 до 3 через пробел ->");
+            x = SCANNER.nextInt() - 1;
+            y = SCANNER.nextInt() - 1;
+        } while (!isCellValid(x, y) || !isCellEmpty(x, y));
+
+        field[y][x] = DOT_HUMAN;
+    }
+
+    private static void aiTurn() {
+        int x;
+        int y;
+        do {
+            x = RANDOM.nextInt(fieldSizeX);
+            y = RANDOM.nextInt(fieldSizeY);
+        } while (!isCellEmpty(x, y));
+
+        field[y][x] = DOT_AI;
+    }
+
+    private static boolean checkGame(char dot, String s) {
+
+        if (checkWin(dot)) {
+            System.out.println(s);
+            return true;
+        }
+        if (checkDraw()) {
+            System.out.println("Draw!!!");
+            return true;
+        }
+        return false;
+    }
+
+    private static boolean checkWin(char c) {
+        // hor
+        if (field[0][0] == c && field[0][1] == c && field[0][2] == c) return true;
+        if (field[1][0] == c && field[1][1] == c && field[1][2] == c) return true;
+        if (field[2][0] == c && field[2][1] == c && field[2][2] == c) return true;
+
+        // ver
+        if (field[0][0] == c && field[1][0] == c && field[2][0] == c) return true;
+        if (field[0][1] == c && field[1][1] == c && field[2][1] == c) return true;
+        if (field[0][2] == c && field[1][2] == c && field[2][2] == c) return true;
+
+        // dia
+        if (field[0][0] == c && field[1][1] == c && field[2][2] == c) return true;
+        if (field[0][2] == c && field[1][1] == c && field[2][0] == c) return true;
+        return false;
+    }
+
+    private static boolean checkDraw() {
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                if (isCellEmpty(x, y)) return false;
+            }
+        }
+        return true;
+    }
+
+    private static void initField() {
+        fieldSizeX = 3;
+        fieldSizeY = 3;
+        field = new char[fieldSizeY][fieldSizeX];
+        for (int y = 0; y < fieldSizeY; y++) {
+            for (int x = 0; x < fieldSizeX; x++) {
+                field[y][x] = DOT_EMPTY;
+            }
+        }
+    }
+
+    private static void printField() {
+        System.out.print("+");
+        for (int i = 0; i < fieldSizeX * 2 + 1; i++)
+            System.out.print((i % 2 == 0) ? "-" : i / 2 + 1);
+        System.out.println();
+
+        for (int i = 0; i < fieldSizeY; i++) {
+            System.out.print(i + 1 + "|");
+            for (int j = 0; j < fieldSizeX; j++)
+                System.out.print(field[i][j] + "|");
+            System.out.println();
+        }
+
+        for (int i = 0; i <= fieldSizeX * 2 + 1; i++)
+            System.out.print("-");
+        System.out.println();
+    }
+
+    private static boolean isCellEmpty(int x, int y) {
+        return field[y][x] == DOT_EMPTY;
+    }
+
+    private static boolean isCellValid(int x, int y) {
+        return x >= 0 && x < fieldSizeX && y >= 0 && y < fieldSizeY;
     }
 }
