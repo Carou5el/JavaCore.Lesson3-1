e51532a6a619a695470edf6c6e517b3d893dc605
Carou5el <garrydeepcool@outlook.com>
2021-02-10 10:42:31 +0300

    Add a class for SpiralArray.

---
+++ src/main/java/kulkov/JavaCore/Lesson3_1/SpiralArray.java
@@ -0,0 +1,16 @@
+package kulkov.JavaCore.Lesson3_1;
+
+public class SpiralArray {
+
+    private int xSize;
+    private int ySize;
+
+    private int[][] matrix2D = new int[ySize][xSize];
+
+    public SpiralArray(int xSize, int ySize) {
+        this.xSize = xSize;
+        this.ySize = ySize;
+    }
+
+
+}
